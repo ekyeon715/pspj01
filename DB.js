df045983cffd7791c56df462e25a85dab3d43d0c
ekyeon715 <rkdwldus9636@naver.com>
2024-07-31 09:59:18 +0900

    제목수정

--- DB.js
+++ DB.js
@@ -51,7 +51,7 @@ document.getElementById('search-button').addEventListener('click', () => {
     const query = document.getElementById('search-input').value.toLowerCase();
     const movieCards = document.querySelectorAll('.movie-card');
     movieCards.forEach(card => {
-        const title = card.querySelector('h3').textContent.toLowerCase();
+        const title = card.querySelector('h2').textContent.toLowerCase();
         if (title.includes(query)) {
             card.style.display = 'block';
         } else {
@@ -59,6 +59,7 @@ document.getElementById('search-button').addEventListener('click', () => {
         }
     });
 });
+
 
 
 
